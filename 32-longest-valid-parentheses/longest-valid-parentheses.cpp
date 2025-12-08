6cd81e6e710bc99ff4b140680224a8b859fe2f3f
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-08 21:00:20 +0530

    Time: 3 ms (42.17%) | Memory: 12.1 MB (11.24%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -7,8 +7,12 @@ public:
         // basically use a concept similar to sliding window ..anytime you 
         // encounter a invalid paranthesis, it means we need to start from a index
         // after that, cos it will be pointless to start from any palce before that. 
-        // i.e any valid parenthsis has already been incountered, and we need to start from
+        // i.e any valid parenthsis has already been incountered, and we need to start after
         // the current index i.
+        
+
+        // initially we can start from 0,which is after -1
+        // so we push -1 into the stack
         st.push_back(-1);
         for(int i=0;i<n;i++){
             if(s[i]=='('){
@@ -16,11 +20,12 @@ public:
             }
             else st.pop_back();
             if(st.size()==0){
-                // start from the index i;
-                // start fresh
+                // start fresh after the index i;
                 st.push_back(i);
             }
             else{
+                // what is the length after the last start position
+                // st.top()
                 maxLen = max(maxLen, i-st.back());
             }
         }
