a762028ddc0474662e896032ebb4614c84c429ec
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-09 15:33:52 +0530

    Time: 54 ms (53.97%) | Memory: 18.7 MB (67.94%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    int lowerBound(vector<int>& nums, int target){
-        int start = 0, end = nums.size()-1;
-        int ans = 0;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]>=target){
-                // potential ans
-                ans = mid;
-                end = mid-1;
-            }
-            else start = mid+1;
-        }
-        return ans;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size(), m = matrix[0].size();
-        for(int i=0;i<n;i++){
-            int indx = lowerBound(matrix[i], target);
-            if(matrix[i][indx]==target)return true;
+        int row = 0, col = m-1;
+        while(row<n and col>=0){
+            if(matrix[row][col]==target)return true;
+            if(matrix[row][col]>target)col--;
+            else row++;
         }
         return false;
     }
