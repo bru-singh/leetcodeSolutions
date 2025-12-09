474ace39979f21c888612602fb848c962c9e1dbe
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-09 13:03:29 +0530

    Time: 53 ms (98.54%) | Memory: 198.2 MB (98.91%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -1,14 +1,22 @@
 class Solution {
+private:
+static const int m = 1e5+1;
+int mod = 1e9+7;
+int leftCount[m], rightCount[m];
 public:
-    int mod = 1e9+7;
     int specialTriplets(vector<int>& nums) {
-        unordered_map<int,int> leftCount, rightCount;
+        memset(leftCount, 0 , sizeof(leftCount));
+        memset(rightCount, 0 , sizeof(rightCount));
         int n = nums.size();
         for(int i=n-1;i>=0;i--)rightCount[nums[i]]++;
         long long res = 0;
         for(int i=0;i<n;i++){
             rightCount[nums[i]]--;
             int specialNum = 2*nums[i];
+            if(specialNum>m){
+                leftCount[nums[i]]++;
+                continue;
+            }
             int left = leftCount[specialNum]%mod;
             int right = rightCount[specialNum]%mod;
             res = (res + (1ll*left*right)%mod)%mod;
