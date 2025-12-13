34fe34b78a2b6e64f0452cd1a147cc88889ede7e
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-14 00:29:51 +0530

    Time: 11 ms (57.92%) | Memory: 22.2 MB (15.16%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -2,12 +2,22 @@ class Solution {
 public:
     int count1DArray(vector<int> &mat){
         int n = mat.size();
-        int count =0, res =0;
+        stack<int> st;
+        int res = 0;
+        vector<int> sum(n,0);
         for(int i=0;i<n;i++){
-            if(mat[i]==0)count=0;
-            else count++;
-            res+=count;
+            while(!st.empty() and mat[st.top()]>=mat[i])st.pop();
+            if(!st.empty()){
+                int prevIndx = st.top();
+                sum[i]=sum[prevIndx];
+                sum[i]+=mat[i]*(i-prevIndx);
+            }
+            else{
+                sum[i] = mat[i]*(i+1);
+            }
+            st.push(i);
         }
+        for(auto it: sum)res+=it;
         return res;
     }
     int numSubmat(vector<vector<int>>& mat) {
@@ -27,16 +37,31 @@ public:
         // (that's why we use &). So overall, the idea is to "compress" the 2D array to the 1D array,
         // and apply 1D array method on it, while trying all heights up to down.
 
-        int n = mat.size(), m = mat[0].size();
+        // int n = mat.size(), m = mat[0].size();
+        // int res = 0;
+        // for(int startRow = 0; startRow<n;startRow++){
+        //     vector<int> colVec(m,1);
+        //     for(int endRow =startRow; endRow<n;endRow++){
+        //         for(int col=0;col<m;col++){
+        //             colVec[col]&= mat[endRow][col];
+        //         }
+        //         res += count1DArray(colVec);
+        //     }
+        // }
+        // return res;
+
+        // USING STACK
+        int n = mat.size(), m =mat[0].size();
         int res = 0;
-        for(int startRow = 0; startRow<n;startRow++){
-            vector<int> colVec(m,1);
-            for(int endRow =startRow; endRow<n;endRow++){
-                for(int col=0;col<m;col++){
-                    colVec[col]&= mat[endRow][col];
+        vector<int> heights(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1){
+                    heights[j]+=1;
                 }
-                res += count1DArray(colVec);
+                else heights[j]=0;
             }
+            res += count1DArray(heights);
         }
         return res;
     }
