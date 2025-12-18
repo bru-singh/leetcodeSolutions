e8418fc6597cac151a99933170b5745ce6352598
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-18 23:47:59 +0530

    Time: 74 ms (50.37%) | Memory: 92.5 MB (49.36%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // choices: 
+    // 0: no pngoing transactions we can start a new transaction
+    // 1: an ongoing transection going on, only can sell or skip
+
+    // changing: 
+    // indx, choice, k 
+    int dp[100001][3][3];
+    int recc(vector<int>&prices, int indx, int choice, int k){
+        if(indx==prices.size())return 0;
+        if(dp[indx][choice][k]!=-1e9)return dp[indx][choice][k];
+
+        // each day we can do:
+        // skip
+        int not_take = recc(prices, indx+1, choice, k);
+        // not skip: 
+        // 1. sell ( if baught before )
+        int sell = -1e9,buy = -1e9;
+        if(k>0 and choice==1)sell = prices[indx] + recc(prices, indx+1, 0, k-1);
+        // 2. buy (if no transection)
+        if(choice == 0 )buy = -prices[indx] + recc(prices, indx+1, 1, k);
+        return dp[indx][choice][k] = max(max(buy, sell), not_take);
+    }
+    int maxProfit(vector<int>& prices) {
+        // at any index, we have the choices: 
+        // if choice == 0:
+        // 1. skip the index
+        // 2. buy at this index, and set the choice to 1
+        // if choice == 1;
+        // 1. sell at this index
+        for(int i=0;i<prices.size();i++){
+            dp[i][0][0]=-1e9;dp[i][0][1]=-1e9;dp[i][0][2]=-1e9;
+            dp[i][1][0]=-1e9;dp[i][1][1]=-1e9;dp[i][1][2]=-1e9;
+            dp[i][2][0]=-1e9;dp[i][2][1]=-1e9;dp[i][2][2]=-1e9;
+        }
+        return recc(prices, 0,0,2);
+    }
+};
