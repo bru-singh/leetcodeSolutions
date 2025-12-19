54584482334b387f42177476e2084d73bf7b30e0
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-19 21:36:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (58.43%) - LeetSync

--- 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int numTimesAllBlue(vector<int>& flips) {
-        int n= flips.size(), ans =0;
-        string binary, prefix; 
+        int n= flips.size(), ans=0;
+        // string binary, prefix; 
+        // for(int i =0;i<n;i++){
+        //     flips[i]-=1;binary+="0";prefix+="0";
+        // }
+        int runningSum=0, total=0;
         for(int i =0;i<n;i++){
-            flips[i]-=1;binary+="0";prefix+="0";
-        }
-        for(int i =0;i<n;i++){
-            binary[flips[i]]='1';
-            prefix[i]='1';
-            if(binary==prefix)ans++;
+            runningSum += flips[i];
+            total +=i+1;
+            if(runningSum == total)ans++;
         }
         return ans;
 
