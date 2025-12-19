62156a303e3360906af889fcec35cd861782f93c
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-19 12:06:02 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (89.29%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -4,7 +4,9 @@ public:
         int left = 0, right = 0;
         int n = nums.size();
         while(right<n){
-            if(nums[right]%2==0){
+            // if(nums[right]%2==0){
+            // use bitmask instead for faster computation
+            if((nums[right]&0x1)==0){
                 swap(nums[left], nums[right]);
                 left++;
             }
