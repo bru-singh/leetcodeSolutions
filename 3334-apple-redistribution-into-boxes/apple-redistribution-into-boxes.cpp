c04844a93dbe7ad7bc0bb420d8dec336f9d335a5
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-24 23:52:39 +0530

    Time: 7 ms (1.01%) | Memory: 34.9 MB (5.29%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -1,14 +1,38 @@
+#include<numeric>
 class Solution {
 public:
-    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
-        int sum = 0, ans =0; 
-        for(auto it: apple)sum+=it;
-        sort(capacity.rbegin(), capacity.rend());
-        for(auto it: capacity){
-            sum-=it;
-            ans++;
-            if(sum<=0)return ans;
+    vector<int> countingSort(vector<int>&capacity){
+        int n = capacity.size();
+        int sum = accumulate(capacity.begin(), capacity.end(),0);
+        vector<int> counting(sum+1), ans(n);
+        for(int i=0;i<n;i++)counting[capacity[i]]++;
+        for(int i=0;i<sum;i++)counting[i+1]+=counting[i];
+        for(int i=n-1;i>=0;i--){
+            ans[counting[capacity[i]]-1]= capacity[i];
+            counting[capacity[i]]--;
         }
-        return -1;
+        return ans;
+    }
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        // int sum = 0, ans =0; 
+        // for(auto it: apple)sum+=it;
+        // sort(capacity.rbegin(), capacity.rend());
+        // for(auto it: capacity){
+        //     sum-=it;
+        //     ans++;
+        //     if(sum<=0)return ans;
+        // }
+        // return -1;
+
+        // we can do this linearly as well by using counting sort 
+        // since the range is small only till 50
+        int sum =0, ans = 0;
+        for(auto it: apple)sum+=it;
+        vector<int> sorted = countingSort(capacity);
+        for(int i=sorted.size()-1;i>=0;i--){
+            sum-=sorted[i];ans++;
+            if(sum<=0)break;
+        }
+        return ans;
     }
 };
