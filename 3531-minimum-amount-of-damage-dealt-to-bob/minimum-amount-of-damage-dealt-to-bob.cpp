4a5cfe2ca43334ba634a5bff6217ec7f4521fe3c
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-26 16:24:05 +0530

    Time: 47 ms (85.98%) | Memory: 148.6 MB (39.25%) - LeetSync

---
+++ 3531-minimum-amount-of-damage-dealt-to-bob/minimum-amount-of-damage-dealt-to-bob.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long minDamage(int power, vector<int>& damage, vector<int>& health) {
+        for(auto &h: health)h= ceil((double)h/(1.0*power));
+        int totalDam = accumulate(damage.begin(), damage.end(), 0);
+        vector<pair<int,int>> arr; 
+        for(int i=0;i<damage.size();i++)arr.push_back({damage[i], health[i]});
+        sort(arr.begin(), arr.end(), [](pair<int,int> &a, pair<int,int> &b){
+            // sum1= a.first/a.second;
+            // sum2= b.first/b.second;
+            // sum1>sum2;
+            // a.first/a.second > b.first/b.second
+            // a.first * b.second > b.first * a.second;
+            long long  x= a.first*1ll*b.second;
+            long long y= b.first*1ll*a.second;
+            return x>y;
+        });
+        long long sum = 0;
+        long long days = 0;
+        for(int i=0;i<arr.size();i++){
+            sum+= arr[i].first*(arr[i].second + days);
+            days +=arr[i].second;
+        }
+        return sum;
+
+    }
+};
