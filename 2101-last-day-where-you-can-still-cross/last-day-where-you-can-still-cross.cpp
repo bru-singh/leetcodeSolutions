2ee1609239632c25a60f794385e4a9787bd54fd3
Brijesh Singh <229brijeshsingh@gmail.com>
2025-12-31 13:54:58 +0530

    Time: 264 ms (42.67%) | Memory: 194.1 MB (47.64%) - LeetSync

---
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool bfs(vector<vector<int>>& matrix){
+        int n = matrix.size(), m = matrix[0].size();
+        vector<pair<int,int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        queue<pair<int,int>> graphQueue;
+        for(int i=0;i<matrix[0].size();i++){
+            if(matrix[0][i]==0){
+                graphQueue.push({0,i});
+                matrix[0][i]=1;
+            }
+        }
+        while(!graphQueue.empty()){
+            auto curr = graphQueue.front();
+            graphQueue.pop();
+            if(curr.first==n-1)return true;
+            for(auto it: dir){
+                int x = curr.first + it.first;
+                int y = curr.second + it.second;
+                if(x>=0 and y>=0 and x<n and y<m 
+                and matrix[x][y]!=1){
+                    graphQueue.push({x,y});
+                    matrix[x][y]=1;
+                }
+            }
+        }
+        return false;
+    }
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        // fill the matrix with the cells, and move across the cells
+        // in a binary search manner(from day 1 to day n-1)
+        int start = 0; 
+        int end = cells.size()-1;
+        int ans = 0;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            vector<vector<int>> matrix(row, vector<int>(col, 0));
+            for(int i=0;i<=mid;i++){
+                matrix[cells[i][0]-1][cells[i][1]-1]=1;
+            }
+            if(bfs(matrix)){
+                ans = mid;
+                start= mid+1;
+            }
+            else end = mid-1;
+        }
+        return ans+1;
+    }
+};
