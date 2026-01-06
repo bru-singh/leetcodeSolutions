d1843f2d6b83e75e86b3bdc789897779e905e2ce
Brijesh Singh <229brijeshsingh@gmail.com>
2026-01-06 10:54:12 +0530

    Time: 12 ms (10.80%) | Memory: 109.5 MB (30.13%) - LeetSync

---
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        // basically we need to find the level wise sum
+        // and return the level with the maximum sum, 
+        // which is the smallest lvl i.e if we encounter a 
+        // sum strictly greater than the curr then only we update
+
+        // do bfs
+        int lvl = 0;
+        pair<int,int> maxSumLevel = {INT_MIN,0};
+        queue<TreeNode*> treeQueue;
+        treeQueue.push(root);
+        while(!treeQueue.empty()){
+            int n = treeQueue.size();
+            int sum = 0;lvl++;
+            for(int i=0;i<n;i++){
+                auto curr = treeQueue.front();
+                treeQueue.pop();
+                sum+=curr->val;
+                if(curr->left)treeQueue.push(curr->left);
+                if(curr->right)treeQueue.push(curr->right);
+            }
+            if(sum>maxSumLevel.first){
+                maxSumLevel.first=sum;
+                maxSumLevel.second=lvl;
+            }
+        }
+        return maxSumLevel.second;
+    }
+};
